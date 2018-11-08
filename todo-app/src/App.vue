11c1ca354b5403d103efbf4c27c83d64bd01de63
Alpha Chau <chaua0927@gmail.com>
2018-11-08 08:01:36 -0500

    Added UI details to TodoList, considered uniqid for rendering key

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <img src="./assets/logo.png">
-    <todo-list></todo-list>
+    <todo-list v-bind="todos"></todo-list>
   </div>
 </template>
 
 
 <script>
 import TodoList from './components/TodoList';
+//const uniqid = require('uniqid');
+
 export default {
   name: 'App',
   components: {
@@ -17,20 +19,28 @@ export default {
   data() {
     return {
       todos: [{
+        id: 0, //uniqid(),
         title: 'Todo A',
         project: 'Project A',
         isDone: false,
       }, {
+        id: 1, //uniqid(),
         title: 'Todo B',
         project: 'Project B',
-        isDone: false,
+        isDone: true,
       }, {
+        id: 2, //uniqid(),
         title: 'Todo C',
         project: 'Project C',
         isDone: false,
-      }]
-    }
-  }
+      }, {
+        id: 3, //uniqid(),
+        title: 'Todo C',
+        project: 'Project C',
+        isDone: false,
+      }],
+    };
+  },
 };
 </script>
 
